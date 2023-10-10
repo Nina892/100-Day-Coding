439447c575898fa752d7710db008a0cec11ad785
Nina892 <ninaa883867@gmail.com>
2023-10-10 13:13:39 +0800

    Day 002

---
+++ Day002.java
@@ -0,0 +1,14 @@
+/**
+ * Day002
+ */
+public class Day002 {
+
+    public static void main(String[] args) {
+        int a = 15;
+        double b = 0.2;
+        double c = a + b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
