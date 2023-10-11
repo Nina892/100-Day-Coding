66679799d56faa69bc6ee6003dd024718432c7b6
Nina892 <ninaa883867@gmail.com>
2023-10-11 18:05:46 +0800

    Day 003

---
+++ Day003.java
@@ -0,0 +1,16 @@
+public class Day003 {
+    public static void main(String[] args) {{
+            String nama = "nina";
+            int umur = 21;
+            float tgi_bdn = 1.55f;
+            double brt_bdn = 5.5;
+            char ukuran_baju = 'L';
+            System.out.println(nama);
+            System.out.println(umur);
+            System.out.println(tgi_bdn);
+            System.out.println(brt_bdn);
+            System.out.println(ukuran_baju);
+        
+    }
+    }
+}
