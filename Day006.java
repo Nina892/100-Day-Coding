d73a329103933b703c9f1554ccc3d8a8e6404f5d
Nina892 <ninaa883867@gmail.com>
2023-10-14 20:51:01 +0800

    Day 006

---
+++ Day006.java
@@ -0,0 +1,11 @@
+public class Day006{
+    public static void main(String[] args) {
+       int umur =20;
+        if (umur <18){
+            System.out.println("kamu tidak boleh membuka situs ini!");
+        }
+        else{
+            System.out.println("selamat datang di welcome");
+        }
+    }
+}
