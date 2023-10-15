ddebf6cd123bde8d2ea2644b5ab109da83c301fa
Nina892 <ninaa883867@gmail.com>
2023-10-16 01:29:05 +0800

    Day 007

---
+++ Day007.java
@@ -0,0 +1,13 @@
+public class Day007 {
+
+ public static void main(String[] args) {
+    char var1;
+    char var2;
+    
+    var1 = 'a';
+    var2 = 'Z';
+    
+    System.out.println(var1);
+    System.out.println(var2);
+}
+}
