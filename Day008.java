5ba3b13876835a1c05098d1271c32a923f67a486
Nina892 <ninaa883867@gmail.com>
2023-10-16 20:53:09 +0800

    Day008

---
+++ Day008.java
@@ -0,0 +1,18 @@
+public class Day008 {
+    public static void main(String[] args){
+     String nama,alamat;
+     int usia;
+     double tinggi;
+
+     nama ="nina";
+     alamat="tutar";
+     usia =20;
+     tinggi =155.5;
+
+     System.out.println("nama saya:" + nama);
+     System.out.println("alamat saya:" + alamat);
+     System.out.println("saya berusia:"+ usia +"thn");
+     System.out.println("saya memiliki tinggi badan:"+ tinggi + "cm");
+    
+}
+}
