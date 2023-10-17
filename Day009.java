6be4c9db9c30c3b9452725df0339d9e1fb4fba80
Nina892 <ninaa883867@gmail.com>
2023-10-17 23:48:20 +0800

    Day 009

---
+++ Day009.java
@@ -0,0 +1,19 @@
+public class Day009 {
+    public static void main(String[] args) {
+     {
+        int a = 2;
+        if(a==1){
+            System.out.println("Nilai a =1");
+        }
+        else if(a==2){
+            System.out.println("Nilai a =2");
+        }
+        else if(a==3){    
+            System.out.println("Nilai a =3");
+        }
+        else{
+            System.out.println("Nilai a tiak terdeteksi");
+  }
+ } 
+ }
+}
