dd5847d89430d1f4e8c476fdc89216dd606c66f7
Nina892 <ninaa883867@gmail.com>
2023-10-18 22:30:00 +0800

    Day 0010

---
+++ Day0010.java
@@ -0,0 +1,11 @@
+public class Day0010 {
+    public static void main(String[] args) {
+         int [][] data = new int [0][0];
+         for(int a=0;a<4;a++){
+            for(int b=0;b<4;b++){
+                System.out.println(+a+" "+ a);
+            }
+            System.out.println("");
+         }
+    }
+}
