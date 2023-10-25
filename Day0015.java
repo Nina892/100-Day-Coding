35435726a6bc8d5a000a2956c6b3040a04e55a30
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-10-25 20:43:34 +0800

    Menentukan index yg dipilih

---
+++ Day0015.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args){
+    //Deklarasi array
+    String [] mahasiswa = {"Rini","Dian","Ana","Putra"};
+
+    //Menampilkan isi array pada indeks ke-2
+    System.out.println(mahasiswa[2]);
+         
+    }
+ }
+ 
