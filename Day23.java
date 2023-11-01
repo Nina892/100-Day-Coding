9292cfa84d6b96c4493d4f36316252e2b565bd78
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-11-01 21:44:16 +0800

    Nonprimitif

---
+++ Day23.java
@@ -0,0 +1,20 @@
+public class NonPrimitifExample {
+    public static void main(String[] args) {
+        // Tipe data non-primitif: String
+        String nama = "Nina";
+
+        // Tipe data non-primitif: Integer
+        Integer umur = 21;
+
+        // Tipe data non-primitif: Double
+        double jajan = 200.0;
+
+        // Tipe data non-primitif: Boolean
+        Boolean sudahMenikah = false;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("jajan: " +  jajan);
+        System.out.println("Sudah Menikah: " + sudahMenikah);
+    }
+}
