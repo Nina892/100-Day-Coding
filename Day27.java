6805585e7723931fe39bf2b1ff8d5a54ebc64065
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-11-04 09:31:58 +0800

    Short

---
+++ Day27.java
@@ -0,0 +1,13 @@
+public class ShortExample {
+    public static void main(String[] args) {
+        // Mendefinisikan variabel short
+        short nilai1 = 100;
+        short nilai2 = 200;
+
+        // Melakukan operasi penjumlahan
+        short hasilPenjumlahan = (short)(nilai1 + nilai2);
+
+        // Menampilkan hasil penjumlahan
+        System.out.println("Hasil penjumlahan: " + hasilPenjumlahan);
+    }
+}
