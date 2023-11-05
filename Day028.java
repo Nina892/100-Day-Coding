1e4fdacc6c875ca9bfa82c1be7c1ecdba4f87507
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-11-05 22:51:45 +0800

    Nonprimitif karakter

---
+++ Day028.java
@@ -0,0 +1,11 @@
+public class Day028{
+    public static void main(String[] args) {
+        String kata = "Halo, Nina!";
+        char karPertama = kata.charAt(0);
+        char karKedua = kata.charAt(6);
+        System.out.println("Karakter pertama: " + karPertama);
+        System.out.println("Karakter kelima: " + karKedua);
+        String sapaan = "Selamat " + "pagi";
+        System.out.println(sapaan);
+    }
+}
