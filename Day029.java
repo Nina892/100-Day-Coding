ca63cca38105453865b7e0943a8da468eae7c11e
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-11-06 23:31:17 +0800

    Double

---
+++ Day029.java
@@ -0,0 +1,18 @@
+public class Day029 {
+    public static void main(String[] args) {
+        double number1 = 5.5;
+        double number2 = 2.0;
+        
+        // Melakukan operasi aritmatika non-primitif dengan tipe data double
+        double tambah = number1 + number2;
+        double kurang= number1 - number2;
+        double kali = number1 * number2;
+        double bagi = number1 / number2;
+        
+        // Menampilkan hasil operasi
+        System.out.println("Penjumlahan: " + tambah);
+        System.out.println("Pengurangan: " + kurang);
+        System.out.println("Perkalian: " + kali);
+        System.out.println("Pembagian: " + bagi);
+    }
+}
