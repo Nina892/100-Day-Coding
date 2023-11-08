680d89c2cb63531bae9586cb92101576e27fe2a5
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-11-08 10:26:10 +0800

    Konversi string ke tipe data primitif

---
+++ Day031.java
@@ -0,0 +1,12 @@
+public class Day031{
+    public static void main(String[] args) {
+        // String yang akan diubah ke int
+        String strNumber = "20";
+
+        // Mengkonversi String ke int
+        int intValue = Integer.parseInt(strNumber);
+
+        // Sekarang, nilai dalam bentuk int
+        System.out.println("Nilai int: " + intValue);
+    }
+}
