8e2aadb5cadc5f40a1adb75e2318eb287b7593e6
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-11-13 22:44:40 +0800

    Hitung gaji pokok

---
+++ Day036.java
@@ -0,0 +1,14 @@
+public class Day036{
+    public static void main(String[] args) {
+        Double gajiPokok = 5000.0;  
+        Double tunjangan = 1000.0;  
+        Integer jamKerja = 40;    
+       
+        Double gajiTotal = gajiPokok + tunjangan;
+        
+        System.out.println("Gaji Pokok: Rp" + gajiPokok);
+        System.out.println("Tunjangan: Rp" + tunjangan);
+        System.out.println("Jam Kerja: " + jamKerja + " jam");
+        System.out.println("Gaji Total: Rp" + gajiTotal);
+    }
+}
