dd085068fd717cd6ee11e70422e7a7ff4a67882c
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-11-25 19:08:48 +0800

    Percabangan

---
+++ Day048.java
@@ -0,0 +1,15 @@
+public class Day047{
+    public static void main(String[] args) {
+        // Mendeklarasikan variabel
+        int angka = 10;
+
+        // Percabangan if-else
+        if (angka > 0) {
+            System.out.println("Angka positif");
+        } else if (angka < 0) {
+            System.out.println("Angka negatif");
+        } else {
+            System.out.println("Angka nol");
+        }
+    }
+}
