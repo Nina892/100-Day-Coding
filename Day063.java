8fdecd77a290415c6c84b84ccdfb7aaa4409f812
Nina892 <100191900+Nina892@users.noreply.github.com>
2023-12-25 23:57:49 +0800

    Add files via upload

---
+++ Day063.java
@@ -0,0 +1,10 @@
+public class Day063 {
+
+    public static void main(String[] args) {
+        String[] buah = {"Apel", "Jeruk", "Mangga", "Anggur", "Pir"};
+        System.out.print("Nama-nama buah: ");
+        for (String namaBuah : buah) {
+            System.out.print(namaBuah + " ");
+        }
+    }
+}
