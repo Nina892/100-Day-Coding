81852be09ce43e0bd579e067b8f8807cdc9b48b8
Nina892 <100191900+Nina892@users.noreply.github.com>
2024-01-18 21:29:43 +0800

    Add files via upload

---
+++ Day072.java
@@ -0,0 +1,11 @@
+public class Day072 {
+    public static void main(String[] args) {
+        byte nilaiAwal = 1;
+        byte batas = 5;
+        for (byte i = nilaiAwal; i <= batas; i++) {
+            System.out.println("Iterasi ke-" + i);
+        }
+    }
+}
+
+	
